fcddb37fb84250db7ccff7420263ea42eb558c46
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-17 12:37:52 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Christian Manzaraz on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
