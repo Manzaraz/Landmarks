24648a5b1519599299e5acce764b22c19484c08e
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-19 19:17:33 -0300

    Section 3: Draw the badge symbol

---
+++ Landmarks/Views/RotateBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Christian Manzaraz on 19/04/2024.
+//
+
+import SwiftUI
+
+struct RotateBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotateBadgeSymbol(angle: Angle(degrees: 5))
+}
