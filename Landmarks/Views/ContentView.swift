9324be121a3342e5eb23b37a37cd6924364cd9e3
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-18 21:44:51 -0300

    Building list and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Christian Manzaraz on 17/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
