6e19bb3668e35089c360983dfb849bd7842d786f
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-19 12:58:44 -0300

    Section 2: Draw the badge background

---
+++ Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Christian Manzaraz on 19/04/2024.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
