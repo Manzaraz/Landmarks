8e0f73b5d49b9815890b6b9c5b671a0968fb5096
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-22 12:38:30 -0300

    Section 1 Add a watchOS target To create a watchOS app, start by adding a watchOS target to the project. Xcode adds groups and files for the watchOS app to your project, along with the schemes needed to build and run the app.

---
+++ watchLandmark Watch App/watchLandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchLandmarkApp.swift
+//  watchLandmark Watch App
+//
+//  Created by Christian Manzaraz on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct watchLandmark_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
