48a5c2ba53b441105760f379fad0db1267c6076e
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-22 00:37:16 -0300

    Section 3 Track the page in a SwiftUI view’s state To prepare for adding a custom UIPageControl, you need a way to track the current page from within PageView. To do this, you’ll declare a @State property in PageView, and pass a binding to this property down to the PageViewController view. The PageViewController updates the binding to match the visible page.

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
     
     var body: some View {
-        PageViewController(pages: pages)
-            .aspectRatio(3 / 2, contentMode: .fit)
+        VStack {
+            PageViewController(pages: pages, currentPage: $currentPage)
+                .aspectRatio(3 / 2, contentMode: .fit)
+            
+            Text("Current Page: \(currentPage)")
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
+
 
 #Preview {
     PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
